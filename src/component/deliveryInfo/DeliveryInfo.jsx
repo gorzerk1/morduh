baea055edaa36e04db6963c45e4590c364cf44d3
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-02-26 19:44:44 +0200

    small update
    small update

--- src/component/deliveryInfo/DeliveryInfo.jsx
+++ src/component/deliveryInfo/DeliveryInfo.jsx
@@ -11,6 +11,20 @@ function DeliveryInfo(){
     { name: 'deliveryRequest', label: 'הערות לשליח', value: '', fit: "3" },
   ]);
 
+  const [formData, setFormData] = useState([])
+
+  const cssFormData = (
+              <div className='deliveryInfo--popDown--line'>
+                <div>קומה:{formData.floorNum}</div>
+                <div>דירה: {formData.homeNum}</div>
+                <div>כניסה: {formData.enterPoint}</div>
+                <div>בית: {formData.streetNum}</div>
+                <div>רחוב: {formData.streetName}</div>
+              </div>
+
+  )
+
+
   function handleInputChange(event) {
     const { name, value } = event.target
     setFormValues(values => values.map(field => {
@@ -22,12 +36,22 @@ function DeliveryInfo(){
     }))
   }
 
+  function handleSubmit(event){
+    event.preventDefault();
+    const newFormData = {};
+    formValues.forEach((field) => {
+      newFormData[field.name] = field.value;
+    });
+    setFormData(newFormData);
+    console.log('Form data:', newFormData);
+  }
+
   const allInputs = formValues.map(field => {
     const classname = `deliveryInfo__grid--col${field.fit ? `${field.fit}` : ''}`
     return (
         <div className={classname}>
           <label className='deliveryInfo--textbar'>{field.label}</label>
-          <input type="text" name={field.name} value={field.value} onChange={handleInputChange} />
+          <input name={field.name} value={field.value} onChange={handleInputChange} />
         </div>
     )
   })
@@ -41,10 +65,13 @@ function DeliveryInfo(){
         <img className='deliveryInfo--body__close' src="../../../order/close.png" alt="" />
         <div className='deliveryInfo--container--info'>לאן המשלוח</div>
         
-        <form className='deliveryInfo--grid'>
+        <form onSubmit={handleSubmit} className='deliveryInfo--grid'>
           <div className='deliveryInfo--savedInfo'>
             <img src="../../../order/downArrow.png" alt="" />
-            <div>רחוב: הפסגה בית: 36 קומה: 0 דירה: 5 כניסה: 0</div>
+            <div>לחץ בשביל לראות כתובות שמורות</div>
+            <div className='deliveryInfo--popDown'>
+              {formData.length === 0 ? '' : cssFormData}
+            </div>
           </div>
           {allInputs}
           <div className='deliveryInfo--checkBox'>

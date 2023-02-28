ee14d53ff72bb58584d3f73721500ccaa11f19f6
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-02-28 17:26:59 +0200

    small order update
    small order update

--- src/component/deliveryInfo/DeliveryInfo.jsx
+++ src/component/deliveryInfo/DeliveryInfo.jsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react'
 
 function DeliveryInfo(){
   const [formValues, setFormValues] = useState([
-    { name: 'streetNum', label: 'בית', value: ''},
     { name: 'streetName', label: 'רחוב', value: '', fit: "2" },
+    { name: 'streetNum', label: 'בית', value: ''},
     { name: 'enterPoint', label: 'כניסה', value: ''},
     { name: 'homeNum', label: 'דירה', value: ''},
     { name: 'floorNum', label: 'קומה', value: ''},
@@ -12,17 +12,10 @@ function DeliveryInfo(){
   ]);
 
   const [formData, setFormData] = useState([])
+  const [popDown, setPopDown] = useState(false)
+  const [isSaved, setIsSaved] = useState(false)
+  const [tempFormData, setTempFormData] = useState(null)
 
-  const cssFormData = (
-              <div className='deliveryInfo--popDown--line'>
-                <div>קומה:{formData.floorNum}</div>
-                <div>דירה: {formData.homeNum}</div>
-                <div>כניסה: {formData.enterPoint}</div>
-                <div>בית: {formData.streetNum}</div>
-                <div>רחוב: {formData.streetName}</div>
-              </div>
-
-  )
 
 
   function handleInputChange(event) {
@@ -38,12 +31,22 @@ function DeliveryInfo(){
 
   function handleSubmit(event){
     event.preventDefault();
-    const newFormData = {};
+    const newFormData = {}
     formValues.forEach((field) => {
-      newFormData[field.name] = field.value;
-    });
-    setFormData(newFormData);
-    console.log('Form data:', newFormData);
+      newFormData[field.name] = field.value
+    })
+    if (isSaved) {
+      setFormData((prevFormData) => [...prevFormData, newFormData])
+    } else if (!isSaved) {
+      setTempFormData(newFormData)
+    }
+    console.log(tempFormData)
+  }
+
+  function removeFormData(index) {
+    const updatedFormData = [...formData]
+    updatedFormData.splice(index, 1)
+    setFormData(updatedFormData)
   }
 
   const allInputs = formValues.map(field => {
@@ -56,6 +59,23 @@ function DeliveryInfo(){
     )
   })
 
+  const divPopDown = (
+    <div className='deliveryInfo--popDown'>
+      {formData.length === 0 ? '' :
+        formData.slice(0, 3).map((data, index) => (
+          <div className='deliveryInfo--popDown--line' key={index}>
+            <div>רחוב: {data.streetName}</div>
+            <div>בית: {data.streetNum}</div>
+            <div>כניסה: {data.enterPoint}</div>
+            <div>דירה: {data.homeNum}</div>
+            <div>קומה:{data.floorNum}</div>
+            <img src="../../../order/bin.png" className='deliveryInfo--bin' onClick={() => {removeFormData(index)}}/>
+          </div>
+        ))
+      }
+    </div>
+  )
+
 
 
   return (
@@ -67,16 +87,14 @@ function DeliveryInfo(){
         
         <form onSubmit={handleSubmit} className='deliveryInfo--grid'>
           <div className='deliveryInfo--savedInfo'>
-            <img src="../../../order/downArrow.png" alt="" />
-            <div>לחץ בשביל לראות כתובות שמורות</div>
-            <div className='deliveryInfo--popDown'>
-              {formData.length === 0 ? '' : cssFormData}
-            </div>
+          <div>לחץ בשביל לראות כתובות שמורות</div>
+            <img src="../../../order/downArrow.png" alt="" onClick={() => {setPopDown(!popDown)}}/>
+            {popDown ? divPopDown : ''}
           </div>
           {allInputs}
           <div className='deliveryInfo--checkBox'>
             <label>שמור כתובת</label>
-            <input type="checkbox" name="myCheckbox" value="1"/>
+            <input type="checkbox" name="myCheckbox" value="1" onClick={() => setIsSaved(!isSaved)}/>
           </div>
           <button className='deliveryInfo__grid--button'>אישור</button>
           

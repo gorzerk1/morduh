16f65ac93f64293c873338e6ab1e5a2ed2c3c706
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-03-05 19:51:25 +0200

    small update

--- src/component/orderMenu/OrderMenu.jsx
+++ src/component/orderMenu/OrderMenu.jsx
@@ -7,7 +7,17 @@ function OrderMenu(){
   <div className='orderMenu--body'>
     <img className='orderMenu--body__image' src="../../../backgroundAbout2.png"/>
     <div className='orderMenu--container'>
-      asd
+      <div className='orderMenu--foodText'>
+        <div>תפריט</div>
+        <div>ספישייל</div>
+        <div>בורגר</div>
+        <div>טוסט</div>
+        <div>כריך</div>
+        <div>מטוגנים</div>
+        <div>מיקסים</div>
+      </div>
+      <div className='orderMenu--foodMenu'>asd2</div>
+      <div className='orderMenu--foodCart'>asd3</div>
     </div>
   </div>
     )
